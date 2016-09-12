e9e19130de8ceeece03ba15eaa83a7d720488816
aisteron <timotheus@list.ru>
2016-09-12 15:17:10 +0300

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+module.exports = function (grunt)
+{
+	grunt.initConfig({
+		pkg:grunt.file.readJSON('package.json'),
+
+
+	pug: {
+		  compile: {
+		    options: {
+		      data: {
+		        debug: false
+		      }
+		    },
+		    files: {
+		      'index.html': ['dev/index.pug']
+		    }
+		  }
+		},
+
+	less: {
+
+			  production: {
+			    options: {
+			      paths: ['dev/css'],
+			      plugins: [
+			        new (require('less-plugin-autoprefix'))({browsers: ["last 3 versions"]}),
+			        new (require('less-plugin-clean-css'))()
+			      ]
+			    },
+			    files: {
+			      'dev/css/custom.css': 'dev/css/custom.less'
+			    }
+			  }
+			},
+	watch:
+	{
+		css:
+		{
+			files:['dev/css/custom.less', 'dev/css/sections/section2.less'],
+			tasks:['less'],
+			options: {
+		      livereload: true,
+		    }
+		},
+		scripts:
+		{
+			files:['dev/index.pug'],
+			tasks:['pug'],
+			options: {
+		      livereload: true,
+		    }
+		}
+	}				
+		
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-pug');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default',['pug','less','watch'])
+};
